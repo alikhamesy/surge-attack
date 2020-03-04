2ad40f0367a8c85720e688499adcd0ec33f6aa78
alikhamesy <alikhamesy0@gmail.com>
2020-03-03 23:02:00 -0800

    pg test

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const {Client} = require('pg')
 const app = express()
 const port = process.env.PORT || 3000
-
+const client = new Client()
+client.connect()
 app.get('/', (req, res) => {
-  res.send("hello world!")
+  client.query("SELECT NOW()", (err, res1) => {
+    res.send(res1.rows[0])
+  })
 })
-
 app.listen(port, () => {console.log(`app on port ${port}`)})
