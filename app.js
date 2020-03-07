34626d3f1ca00907ca7bdc0828f33025529a304a
alikhamesy <alikhamesy0@gmail.com>
2020-03-06 22:42:41 -0800

    pacdots dev server

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
-const {Client} = require('pg')
+// const {Client} = require('pg')
 const app = express()
 const port = process.env.PORT || 3000
-const client = new Client()
-client.connect()
+// const client = new Client()
+// client.connect()
+
+
+const dots = require('./dots')
+let i = 0;
 app.get('/', (req, res) => {
-  client.query("SELECT NOW()", (err, res1) => {
-    res.send(res1.rows[0])
-  })
+  // client.query("SELECT NOW()", (err, res1) => {
+  //   res.send(res1.rows[0])
+  // })
+  res.send(`reports = ${i}`)
 })
+
+app.get('/pacdots', (req, res) => {
+  i++
+  res.json(dots)
+})
+
 app.listen(port, () => {console.log(`app on port ${port}`)})
